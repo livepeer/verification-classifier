97bd7b49d6b4d51220b786ba4daab198d64fbb59
Rabindranath <rabindranath.andujar@gmail.com>
2019-03-22 11:25:15 +0100

    Adds temporal_psnr metric

--- scripts/video_metrics.py
+++ scripts/video_metrics.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 from scipy.spatial import distance
 from skimage.measure import compare_ssim
+import cv2
 
 class video_metrics:
     def __init__(self, metrics_list, skip_frames, hash_size):
@@ -15,8 +16,8 @@ class video_metrics:
         width = min(img_A.shape[1], img_B.shape[1])
         height = min(img_A.shape[0], img_B.shape[0])
 
-        resized_A = np.resize(img_A, (width, height))
-        resized_B = np.resize(img_B, (width, height))
+        resized_A = cv2.resize(img_A, (width, height))
+        resized_B = cv2.resize(img_B, (width, height))
 
         return resized_A, resized_B
 
@@ -51,14 +52,14 @@ class video_metrics:
         # Return only the first 15 elements of the array
         return hash_array[:15]
 
-    def evaluate_difference_instant(self, frame_list, position_frame):
+    def evaluate_difference_instant(self, frame_list, frame_pos):
         # Function to compute the instantaneous difference between a frame
         # and its subsequent
 
         # Grab the current frame        
-        current_frame = frame_list[position_frame]
+        current_frame = frame_list[frame_pos]
         # Grab the frame skip_frames ahead
-        next_frame = frame_list[position_frame + self.skip_frames]
+        next_frame = frame_list[frame_pos + self.skip_frames]
 
         # Compute the number of different pixels
         total_pixels = current_frame.shape[0] * current_frame.shape[1]
@@ -67,20 +68,38 @@ class video_metrics:
 
         return difference_ratio
     
-    def compute_metrics(self, position_frame, rendition_frame_list, reference_frame):
+    def evaluate_psnr_instant(self, reference_frame, frame_list, frame_pos):
+        # Function to compute the instantaneous difference between a frame
+        # and its subsequent
+
+        # Grab the current frame        
+        current_frame = frame_list[frame_pos]
+        # Grab the frame skip_frames ahead
+        next_frame = frame_list[frame_pos + self.skip_frames]
+
+        scaled_reference, scaled_rendition = self.rescale_pair(reference_frame, next_frame)
+        difference_psnr = self.psnr(scaled_reference, scaled_rendition)
+        return difference_psnr
+
+    def compute_metrics(self, frame_pos, rendition_frame_list, reference_frame):
         rendition_metrics = {}
         for metric in self.metrics_list:
             if metric == 'temporal_difference':
                 # Compute the temporal inter frame difference                
-                rendition_metrics['temporal_difference'] = self.evaluate_difference_instant(rendition_frame_list, position_frame)
+                rendition_metrics['temporal_difference'] = self.evaluate_difference_instant(rendition_frame_list, frame_pos)
+            
+            if metric == 'temporal_psnr':
+                # Compute the temporal inter frame psnr                
+                rendition_metrics['temporal_psnr'] = self.evaluate_difference_instant(rendition_frame_list, frame_pos)
 
-            rendition_frame = rendition_frame_list[position_frame]
+            rendition_frame = rendition_frame_list[frame_pos]
+            
             # Compute the hash of the target frame
             rendition_hash = self.dhash(rendition_frame)
 
             # Extract the dhash for the reference frame            
             reference_hash = self.dhash(reference_frame)
-
+            
             # Compute different distances with the hash
             if metric == 'hash_euclidean':
                 rendition_metrics['hash_euclidean'] = distance.euclidean(reference_hash, rendition_hash)
@@ -89,8 +108,11 @@ class video_metrics:
             if metric == 'hash_cosine':
                 rendition_metrics['hash_cosine'] = distance.cosine(reference_hash, rendition_hash)
 
-            # Compute SSIM and PSNR
+            # Scale the reference and the rendition so they have exactly the same dimensions in order to compare
+            # PSNR and SSIM
             scaled_reference, scaled_rendition = self.rescale_pair(reference_frame, rendition_frame)
+
+            # Compute SSIM and PSNR            
             if metric == 'ssim':
                 rendition_metrics['ssim'] = compare_ssim(scaled_reference, scaled_rendition)
             if metric == 'psnr':
