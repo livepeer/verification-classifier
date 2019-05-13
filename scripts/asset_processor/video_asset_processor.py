c1ebf5a954e99c42ded3fc9283bbae782af84fcb
Rabindranath <rabindranath.andujar@gmail.com>
2019-05-13 14:45:22 +0200

    Adds code documentation

--- scripts/asset_processor/video_asset_processor.py
+++ scripts/asset_processor/video_asset_processor.py
@@ -8,30 +8,45 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from scipy.spatial import distance
 
 class video_asset_processor:
-    def __init__(self, source_path, renditions_paths, metrics_list, duration):
-        
+    # Class to extract and aggregate values from video sequences.
+    # It is instantiated as part of the data creation as well
+    # as in the inference, both in the CLI as in the notebooks
+    def __init__(self, original_path, renditions_paths, metrics_list, duration):
+        # ************************************************************************
         # Initialize global variables
-        self.source_path = source_path
-        self.source = cv2.VideoCapture(self.source_path)
-        self.fps = int(self.source.get(cv2.CAP_PROP_FPS))
-        self.asset_length = int(self.source.get(cv2.CAP_PROP_FRAME_COUNT))
-        self.duration = duration
-        self.skip_frames = 1
-        self.hash_size = 16
-        self.renditions = {}
-        self.metrics = {}
-        self.metrics_list = metrics_list
-        self.video_metrics = video_metrics(self.metrics_list, self.skip_frames, self.hash_size)
-        self.renditions_paths = renditions_paths
-        # Retrieve original rendition dimensions
-        self.height = self.source.get(cv2.CAP_PROP_FRAME_HEIGHT)   
-        self.width = self.source.get(cv2.CAP_PROP_FRAME_WIDTH) 
-        self.dimensions = '{}:{}'.format(int(self.width), int(self.height))
+        # ************************************************************************
 
-    def capture_to_list(self, capture):
-        frame_list = []
-        seconds = 0
-        frame_count = 0
+        
+        self.original_path = original_path                                                      # Stores system path to original asset
+        self.original = cv2.VideoCapture(self.original_path)                                    # Initializes original asset to OpenCV VideoCapture class
+        self.fps = int(self.original.get(cv2.CAP_PROP_FPS))                                     # Frames Per Second of the original asset
+        self.asset_length = int(self.original.get(cv2.CAP_PROP_FRAME_COUNT))                    # Counts number of frames of the asset
+        self.duration = duration                                                                # Establishes how many seconds of the original asset are used
+        self.skip_frames = 1                                                                    # Defines whether to use all frames or leap frog skip_frames frames
+        self.hash_size = 16                                                                     # Size of the hash for frame hash analysis in video_metrics
+        self.renditions = {}                                                                    # Dictionary containing dict of renditions ('frame_list',
+                                                                                                #                                           'dimensions',
+                                                                                                #                                           'ID')
+        self.metrics = {}                                                                       # Dictionary containing dict of metrics
+        self.metrics_list = metrics_list                                                        # List of metrics to be extracted from the asset and its renditions
+        self.video_metrics = video_metrics(self.metrics_list, self.skip_frames, self.hash_size) # Instance of the video_metrics class
+        self.renditions_paths = renditions_paths                                                # List of paths to renditions
+
+        # Retrieve original rendition dimensions
+        self.height = self.original.get(cv2.CAP_PROP_FRAME_HEIGHT)                              # Obtains vertical dimension of the frames of the original
+        self.width = self.original.get(cv2.CAP_PROP_FRAME_WIDTH)                                # Obtains horizontal dimension of the frames of the original 
+        self.dimensions = '{}:{}'.format(int(self.width), int(self.height))                     # Collects both dimensional values in a string
+
+    def capture_to_array(self, capture):
+        # ************************************************************************
+        # Function to convert OpenCV video capture to a list of
+        # numpy arrays for faster processing and analysis
+        # ************************************************************************
+
+        frame_list = []                                                                         # List of numpy arrays
+        seconds = 0                                                                             # Number of seconds processed
+        frame_count = 0                                                                         # Number of frames processed
+        
         # Iterate through each frame in the video
         while capture.isOpened():
             
@@ -47,12 +62,11 @@ class video_asset_processor:
                 
                 frame_count += 1
                 seconds = frame_count / self.fps
-            # Break the loop when frames cannot be taken from source
+            # Break the loop when frames cannot be taken from original
             else:
                 break
             # Break the loop when seconds are longer than defined duration of analysis
             if seconds > self.duration:
-                print('Captured {} seconds ({} frames)'.format(seconds, frame_count))
                 break
         # Clean up memory 
         capture.release()
@@ -62,14 +76,23 @@ class video_asset_processor:
     def compare_renditions_instant(self, frame_pos, frame_list, dimensions, path):
         # Iterate for each given comparable rendition
         frame_metrics = {}
+        # Function to compare pairs of numpy arrays extracting their corresponding metrics.
+        # It basically takes the global original frame at frame_pos and its subsequent to
+        # compare them against the corresponding ones in frame_list (a rendition).
+        # It then extracts the metrics defined in the constructor under the metrics_list.
+        # Methods of comparison are implemented in the video_metrics class
+        # ************************************************************************
 
-        reference_frame = self.source[frame_pos]       
-        next_reference_frame = self.source[frame_pos + self.skip_frames]
-        rendition_frame = frame_list[frame_pos]
-        next_rendition_frame = frame_list[frame_pos + self.skip_frames]
-
-        start_time = time.time()
+        frame_metrics = {}                                                                      # Dictionary of metrics
+        reference_frame = self.original[frame_pos]                                                # Original frame to compare against       
+        next_reference_frame = self.original[frame_pos + self.skip_frames]                        # Original's subsequent frame
+        rendition_frame = frame_list[frame_pos]                                                 # Rendition frame
+        next_rendition_frame = frame_list[frame_pos + self.skip_frames]                         # Rendition's subsequent frame
+        start_time = time.time()                                                                # Profiling start time to assess efficiency of the process
         
+        # Compute the metrics defined in the global metrics_list. Uses the global instance of video_metrics
+        # Some metrics use a frame-to-frame comparison, but other require current and forward frames to extract
+        # their comparative values.
         rendition_metrics = self.video_metrics.compute_metrics(frame_pos, rendition_frame, next_rendition_frame, reference_frame, next_reference_frame)
 
         # Collect processing time
@@ -84,48 +107,87 @@ class video_asset_processor:
         # Identify rendition uniquely by its ID and store metric data in frame_metrics dict
         frame_metrics[path] = rendition_metrics
 
+        # Return the metrics, together with the position of the frame
+        # frame_pos is needed for the ThreadPoolExecutor optimizations
         return rendition_metrics, frame_pos
 
     def compute(self, frame_list, path, dimensions):
         rendition_metrics = {}
-        
-        # Iterate frame by frame
-        frame_pos = 0
-        frames_to_process = []
+        # Function to compare lists of numpy arrays extracting their corresponding metrics.
+        # It basically takes the global original list of frames and the input frame_list
+        # of numpy arrrays to extract the metrics defined in the constructor.
+        # frame_pos establishes the index of the frames to be compared.
+        # It is optimized by means of the ThreadPoolExecutor of Python's concurrent package
+        # for better parallel performance.
+        # ************************************************************************
+
+        rendition_metrics = {}                                                                  # Dictionary of metrics
+        frame_pos = 0                                                                           # Position of the frame
+        frames_to_process = []                                                                  # List of frames to be processed
+
+
+        # Iterate frame by frame and fill a list with their values
+        # to be passed to the ThreadPoolExecutor. Stop when maximum
+        # number of frames has been reached.
         while frame_pos + self.skip_frames < self.duration * self.fps:
-            # Compare the original source against its renditions
             if frame_pos < len(frame_list):
                 frames_to_process.append(frame_pos)
             frame_pos += 1
 
+        # Execute computations in parallel using as many processors as possible
+        # future_list is a dictionary storing all computed values from each thread
         with ThreadPoolExecutor() as executor:
+            # Compare the original asset against its renditions
             future_list = {executor.submit(self.compare_renditions_instant, i, frame_list, dimensions, path): i for i in frames_to_process}
 
+        # Once all frames in frame_list have been iterated, we can retrieve their values
         for future in future_list:
+            # Values are retrieved in a dict, as a result of the executor's process
             result_rendition_metrics, frame_pos = future.result()
+            # The computed values at a given frame
             rendition_metrics[frame_pos] = result_rendition_metrics
 
+        # Return the metrics for the currently processed rendition
         self.metrics[path] = rendition_metrics
 
     def aggregate(self, metrics):
-        metrics_dict = {}
+        # ************************************************************************
+        # Function to aggregate computed values of metrics and renditions into a 
+        # pandas DataFrame.
+        # ************************************************************************
 
-        ## Aggregate dictionary with values into a Pandas DataFrame
+        metrics_dict = {}                                                                       # Dictionary for containing all metrics
+        renditions_dict = {}                                                                    # Dictionary for containing all renditions
+
+        # Aggregate dictionary with all values for all renditions into a Pandas DataFrame
+        # All values are stored and obtained in a per-frame basis, then in a per-rendition
+        # fashion. They need to be rearranged.
+        
+        # First, we combine the frames
         dict_of_df = {k: pd.DataFrame(v) for k, v in metrics.items()}
         metrics_df = pd.concat(dict_of_df, axis=1).transpose().reset_index(inplace=False)
+        # Pandas concat function creates a level_1 and level_2 extra columns. They need to be renamed
         metrics_df = metrics_df.rename(index=str, columns={"level_1": "frame_num", "level_0": "path"})
 
-        renditions_dict = {}
+        # Then we can combine each rendition        
         for rendition in self.renditions_paths:
+            # For the current rendition, we need an empty dictionary
             rendition_dict = {}
+
+            # We have a number of different metrics that have been computed. 
+            # See the list in the video_metrics class
             for metric in self.metrics_list:
-
-                original_df = metrics_df[metrics_df['path']==self.source_path][metric]
+                # Obtain a Pandas DataFrame from the original and build the original time series
+                original_df = metrics_df[metrics_df['path']==self.original_path][metric]
                 original_df = original_df.reset_index(drop=True).transpose().dropna().astype(float)
-
+                # Obtain a Pandas DataFrame from the current rendition and build its time series
                 rendition_df = metrics_df[metrics_df['path']==rendition][metric]
                 rendition_df = rendition_df.reset_index(drop=True).transpose().dropna().astype(float)
 
+                # For those metrics that have a temporal character, we need to make a further aggregation
+                # We are basically using the Manhattan and euclidean distances, and statistically meaningful
+                # values such as mean, max and standard deviation.
+                # The whole time series is also provided for later exploration in the analysis part.
                 if  'temporal' in metric:
                     x_original = np.array(original_df[rendition_df.index].values)
                     x_rendition = np.array(rendition_df.values)
@@ -137,16 +199,23 @@ class video_asset_processor:
                     rendition_dict['{}-mean'.format(metric)] = np.mean(x_rendition)
                     rendition_dict['{}-max'.format(metric)] = np.max(x_rendition)
                     rendition_dict['{}-std'.format(metric)] = np.std(x_rendition)
+
+                # Other metrics do not need time evaluation
                 else:
                     rendition_dict[metric] = rendition_df.mean()
+                
+                # Size is an important feature of an asset, as it gives important information
+                # regarding the potential compression effect
                 rendition_dict['size'] = os.path.getsize(rendition)
+
+            # Store the rendition values in the dictionary of renditions for the present asset                
             renditions_dict[rendition] = rendition_dict
 
-        metrics_dict[self.source_path] = renditions_dict 
+        # Add the current asset values to the global metrics_dict
+        metrics_dict[self.original_path] = renditions_dict 
 
         dict_of_df = {k: pd.DataFrame(v) for k,v in metrics_dict.items()}
         metrics_df = pd.concat(dict_of_df, axis=1).transpose().reset_index(inplace=False)
-        print(metrics_df)  
 
         metrics_df['title'] = metrics_df['level_0']
         attack_series = []
@@ -170,16 +239,20 @@ class video_asset_processor:
         return metrics_df
 
     def process(self):
+        # ************************************************************************
+        # Function to aggregate computed values of metrics 
+        # of iterated renditions into a pandas DataFrame.
+        # ************************************************************************
 
         # Convert OpenCV video captures of original to list
         # of numpy arrays for better performance of numerical computations
-        self.source = self.capture_to_list(self.source)
+        self.original = self.capture_to_array(self.original)
         # Compute its features
-        self.compute(self.source, self.source_path, self.dimensions)
+        self.compute(self.original, self.original_path, self.dimensions, self.fps)
         # Store the value in the renditions dictionary
-        self.renditions['original'] = {'frame_list': self.source,
+        self.renditions['original'] = {'frame_list': self.original,
                                        'dimensions': self.dimensions,
-                                       'ID': self.source_path.split('/')[-2]}
+                                       'ID': self.original_path.split('/')[-2]}
 
         # Iterate through renditions
         for path in self.renditions_paths:
@@ -190,6 +263,7 @@ class video_asset_processor:
                 dimensions = '{}:{}'.format(int(width), int(height))
 
                 # Turn openCV capture to a list of numpy arrays
+                frame_list = self.capture_to_array(capture)
                 frame_list = self.capture_to_list(capture)
                 self.compute(frame_list, path, dimensions)
             except Exception as err:
