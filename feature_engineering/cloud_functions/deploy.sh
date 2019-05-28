bb1c52daa00e54de32bfeb7fb314bbcd223aa2e8
Rabindranath <rabindranath.andujar@gmail.com>
2019-05-28 14:24:44 +0200

    Adds script cleanup to cloud functions deploy.sh

--- feature_engineering/cloud_functions/deploy.sh
+++ feature_engineering/cloud_functions/deploy.sh
@@ -1,3 +1,5 @@
 cp ../../scripts/asset_processor/video_asset_processor.py imports/
 cp ../../scripts/asset_processor/video_metrics.py imports/
 gcloud beta functions deploy measure_asset_http --runtime python37 --trigger-http --memory 2048 --timeout 240
+rm imports/video_asset_processor.py 
+rm imports/video_metrics.py
