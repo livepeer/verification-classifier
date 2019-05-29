6466548655c0fa1f162037634bfb8c57dac95a84
Rabindranath <rabindranath.andujar@gmail.com>
2019-05-29 08:37:21 +0200

    Arranges metrics dataframe in CLI

--- cli/cli.py
+++ cli/cli.py
@@ -30,7 +30,27 @@ def cli(asset, renditions, model_url):
 
     metrics_df = asset_processor.process()
 
-
+    for column in metrics_df.columns:
+        if 'series' in column:
+            metrics_df =  metrics_df.drop([column], axis=1)
+    metrics_df =  metrics_df.drop('title', axis=1)
+    metrics_df =  metrics_df.drop('path', axis=1)
+    metrics_df = metrics_df.drop('attack', axis=1)
+    metrics_df = metrics_df[['dimension', 'fps', 'size', 'temporal_canny-euclidean',
+       'temporal_canny-manhattan', 'temporal_canny-max', 'temporal_canny-mean',
+       'temporal_canny-std', 'temporal_cross_correlation-euclidean',
+       'temporal_cross_correlation-manhattan',
+       'temporal_cross_correlation-max', 'temporal_cross_correlation-mean',
+       'temporal_cross_correlation-std', 'temporal_dct-euclidean',
+       'temporal_dct-manhattan', 'temporal_dct-max', 'temporal_dct-mean',
+       'temporal_dct-std', 'temporal_difference-euclidean',
+       'temporal_difference-manhattan', 'temporal_difference-max',
+       'temporal_difference-mean', 'temporal_difference-std',
+       'temporal_histogram_distance-euclidean',
+       'temporal_histogram_distance-manhattan',
+       'temporal_histogram_distance-max', 'temporal_histogram_distance-mean',
+       'temporal_histogram_distance-std']
+                             ]
     X = np.asarray(metrics_df)
     # make predictions for given data
     y_pred = loaded_model.predict(X)
