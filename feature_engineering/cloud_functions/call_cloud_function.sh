f852c1d6027432a83243597eb42c0b0c5f47e2ef
Rabindranath <rabindranath.andujar@gmail.com>
2019-07-18 16:35:59 +0200

    Increases sleep time for call_cloud_function.sh

--- feature_engineering/cloud_functions/call_cloud_function.sh
+++ feature_engineering/cloud_functions/call_cloud_function.sh
@@ -5,5 +5,5 @@ do
     filename="${filename##*/}"
     gcloud beta functions call measure_asset_http --data '{"name":"'"$filename"'"}' &
     echo "$filename launched"
-    sleep 6
+    sleep 7
 done;
