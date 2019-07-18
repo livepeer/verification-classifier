6b1a651441dc862439dcee91c49c15a67b2ba946
Rabindranath <rabindranath.andujar@gmail.com>
2019-07-18 16:36:19 +0200

    Increases timeout in cloud functions

--- feature_engineering/cloud_functions/deploy.sh
+++ feature_engineering/cloud_functions/deploy.sh
@@ -1,5 +1,5 @@
 cp ../../scripts/asset_processor/video_asset_processor.py imports/
 cp ../../scripts/asset_processor/video_metrics.py imports/
-gcloud beta functions deploy measure_asset_http --runtime python37 --trigger-http --memory 2048 --timeout 360
+gcloud beta functions deploy measure_asset_http --runtime python37 --trigger-http --memory 2048 --timeout 540
 rm imports/video_asset_processor.py 
 rm imports/video_metrics.py
