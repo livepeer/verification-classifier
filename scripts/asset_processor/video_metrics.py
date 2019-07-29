68fcb87a6b5f79ee6d7162aaed018dfcef7aa71e
Rabindranath <rabindranath.andujar@gmail.com>
2019-07-29 09:36:13 +0200

    Adds dynamic time warping dist to video_metrics

--- scripts/asset_processor/video_metrics.py
+++ scripts/asset_processor/video_metrics.py
@@ -1,7 +1,9 @@
+import cv2
 import numpy as np
 import math
+import sys
 from scipy.spatial import distance
-import cv2
+
 from skimage.filters import gaussian
 from sklearn.metrics import mean_squared_error
 from skimage.measure import compare_ssim as ssim
@@ -66,6 +68,50 @@ class video_metrics:
         hash_array = [int(x) for x in str(hash)]
         # Return only the first 15 elements of the array
         return hash_array[:15]
+
+    @staticmethod
+    def dtw_distance(ts_a, ts_b, d = lambda x,y: abs(x-y)):
+        """Returns the DTW similarity distance between two 2-D
+        timeseries numpy arrays.
+
+        Arguments
+        ---------
+        ts_a, ts_b : array of shape [n_samples, n_timepoints]
+            Two arrays containing n_samples of timeseries data
+            whose DTW distance between each sample of A and B
+            will be compared
+        
+        d : DistanceMetric object (default = abs(x-y))
+            the distance measure used for A_i - B_j in the
+            DTW dynamic programming function
+        
+        Returns
+        -------
+        DTW distance between A and B
+        """
+        max_warping_window=10000
+        # Create cost matrix via broadcasting with large int
+        ts_a, ts_b = np.array(ts_a), np.array(ts_b)
+        M, N = len(ts_a), len(ts_b)
+        cost = sys.maxsize * np.ones((M, N))
+
+        # Initialize the first row and column
+        cost[0, 0] = d(ts_a[0], ts_b[0])
+        for i in range(1, M):
+            cost[i, 0] = cost[i-1, 0] + d(ts_a[i], ts_b[0])
+
+        for j in range(1, N):
+            cost[0, j] = cost[0, j-1] + d(ts_a[0], ts_b[j])
+
+        # Populate rest of cost matrix within window
+        for i in range(1, M):
+            for j in range(max(1, i - max_warping_window),
+                            min(N, i + max_warping_window)):
+                choices = cost[i - 1, j - 1], cost[i, j-1], cost[i-1, j]
+                cost[i, j] = min(choices) + d(ts_a[i], ts_b[j])
+
+        # Return DTW distance given window 
+        return cost[-1, -1]
 
     @staticmethod
     def evaluate_difference_instant(current_frame, next_frame):
