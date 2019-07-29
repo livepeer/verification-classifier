6f66ecd0f2d34879e412612e9a5dc42a54bcf64d
Rabindranath <rabindranath.andujar@gmail.com>
2019-07-29 09:37:22 +0200

    Adds dwt and fft computations to metric_processor

--- scripts/modeling_toolbox/metric_processor.py
+++ scripts/modeling_toolbox/metric_processor.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
-
+from scipy import fft
+import pywt
 
 class MetricProcessor:
 
-    def __init__(self, features, learning_type, path, reduced=False, bins=0):
+    def __init__(self, features, learning_type, path, reduced=20000, bins=0):
         self.features = features
         self.learning_type = learning_type
         self.path = path
@@ -17,10 +18,31 @@ class MetricProcessor:
 
         self.info_columns = ['attack_ID', 'title', 'attack']
 
+    @staticmethod
+    def compute_fft(row, column):
+        y = np.fromstring(row[column].replace('[', '').replace(']', ''), 
+                                            dtype=np.float, sep=' ')
+        n = len(y)
+        Y = np.fft.fft(y)/n
+        Y = Y[range(int(n/2))]
+
+        return Y
+
+    @staticmethod
+    def compute_dwt(row, column, nbins):
+        scales = range(1,10)
+        waveletname = 'morl'
+        signal = np.fromstring(row[column].replace('[', '').replace(']', ''), 
+                                            dtype=np.float, sep=' ')
+        coeff, _ = pywt.cwt(signal, scales, waveletname, 1)
+        
+        coeff_ = coeff[:,:nbins]
+        return coeff_[0]
+
     def read_and_process_data(self):
         data = pd.read_csv(self.path)
         if self.reduced:
-            data = data[:20000]
+            data = data[:self.reduced]
         renditions_list = ['attack', '1080p', '720p', '480p', '360p', '240p', '144p']
 
         df = pd.DataFrame(data)
@@ -29,7 +51,7 @@ class MetricProcessor:
         del data
         attack_IDs = []
 
-        for row_index, row in df.iterrows():
+        for _, row in df.iterrows():
 
             if row['attack'] in renditions_list:
                 attack_IDs.append(renditions_list.index(row['attack']))
@@ -40,27 +62,22 @@ class MetricProcessor:
             else:
                 attack_IDs.append(10)
 
-            if self.bins != 0:
-                histogram_range = np.arange(self.bins)
-                for column in df.columns:
-                    if 'series' in column:
-                        for i in histogram_range:
-                            df['{}-hist-{}'.format(column, i)] = 0.0
-                            df['{}-mean-{}'.format(column, i)] = 0.0
-
-                for column in self.series_features_list:
-                    time_series = np.fromstring(row[column].replace('[', '').replace(']', ''), dtype=np.float, sep=' ')
-                    histogram = np.histogram(time_series, bins=histogram_range, density=True)[0]
-                    mean_series = np.array_split(time_series, self.bins)
-
-                    for i in np.arange(len(histogram)):
-                        df.at[row_index, '{}-hist-{}'.format(column, i)] = histogram[i]
-                        df.at[row_index, '{}-mean-{}'.format(column, i)] = mean_series[i].mean()
-
+        if self.bins != 0:
+            for column in self.series_features_list:
+                print(column)
+                for i in range(self.bins):
+                    print(i)
+                    # df['{}-hist-{}'.format(column, i)] = df.apply(lambda row: np.histogram((np.fromstring(row[column].replace('[', '').replace(']', ''), 
+                    #                         dtype=np.float, sep=' ')), self.bins)[0][i], axis=1)
+                    # df['{}-mean-{}'.format(column, i)] = df.apply(lambda row: np.array_split((np.fromstring(row[column].replace('[', '').replace(']', ''), 
+                    #                         dtype=np.float, sep=' ')), self.bins)[i].mean(), axis=1)
+                    df['{}-dwt-{}'.format(column, i)] = df.apply(lambda row: self.compute_dwt(row, column, self.bins)[i], axis=1)
+                
+        
         df['attack_ID'] = attack_IDs
 
         if self.bins != 0:
-            hist_n_means = list(filter(lambda x: '-mean-' in x or '-hist-' in x, list(df)))
+            hist_n_means = list(filter(lambda x: '-mean-' in x or '-hist-' in x or '-dwt-' in x, list(df)))
             self.features.extend(hist_n_means)
 
         df = df.drop(['Unnamed: 0', 'path', 'kind'], axis=1)
@@ -69,9 +86,10 @@ class MetricProcessor:
 
         columns = self.features
         columns.extend(self.info_columns)
+        
         df = df[columns]
         df = df.dropna()
-
+        
         return df
 
     def split_test_and_train(self, df, train_prop=0.8):
