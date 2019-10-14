57569bc58ccec40daceeeaf42fd1877af0cba51b
Rabindranath <rabindranath.andujar@gmail.com>
2019-10-14 16:10:37 +0200

    Adapts cloud functions for psnr and ssim

--- machine_learning/cloud_functions/collect_from_datastore.py
+++ machine_learning/cloud_functions/collect_from_datastore.py
@@ -46,7 +46,7 @@ def initialize():
     print('Initializing...')
     pd.set_option('display.max_colwidth', -1)
 
-    namespace = 'livepeer-verifier-training'
+    namespace = 'livepeer-verifier-QoE'
     client = create_client('epiclabs')
     query = client.query(kind='__kind__',namespace=namespace)
     query.keys_only()
@@ -54,7 +54,7 @@ def initialize():
     inputs_df = pd.DataFrame()
 
     print('Getting inputs...')
-    input_kinds = [entity.key.name for entity in query.fetch() if 'features_input_540' in entity.key.name]
+    input_kinds = [entity.key.name for entity in query.fetch() if 'features_input_60_540' in entity.key.name]
     
     print('Retrieving data from Datastore...')
     for kind in input_kinds:
@@ -64,5 +64,5 @@ def initialize():
         inputs_df = pd.concat([inputs_df, kind_df],axis=0,sort=True, ignore_index=True)
 
         jobs_dict[kind] = inputs_df['title'][inputs_df['kind']==kind]
-    inputs_df.to_csv('data-large.csv')
+    inputs_df.to_csv('data-qoe-large.csv')
 initialize()
