4dcc79aacd3423c54c55a9d27c29cf37bc535f23
Rabindranath <rabindranath.andujar@gmail.com>
2019-10-24 10:01:03 +0200

    Solves rebase conflicts in cli/cli.py

--- cli/cli.py
+++ cli/cli.py
@@ -12,7 +12,6 @@ from verifier import retrieve_model, verify, pre_verify
 @click.argument('model_uri')
 @click.option('--pre_verification_parameters')
 @click.option('--do_profiling', default=0)
-<<<<<<< HEAD
 @click.option('--max_samples', type=int, default=10)
 def cli(source, renditions, model_uri, do_profiling, max_samples):
     # ************************************************************************
@@ -30,64 +29,6 @@ def cli(source, renditions, model_uri, do_profiling, max_samples):
 
     predictions = verify(source, renditions, do_profiling, max_samples, model_dir, model_file)
     results = []
-=======
-def cli(asset, renditions, do_profiling):
-    # Download model from remote url
-    total_start = time.clock()
-    model_url = 'https://github.com/livepeer/verification-classifier/blob/master/machine_learning/output/models/' \
-                'model.tar.gz?raw=true'
-    model_name = 'XGBoost'
-    scaler_type = 'MinMaxScaler'
-    learning_type = 'SL'
-    start = time.clock()
-    download_models(model_url)
-    download_time = time.clock() - start
-    loaded_model = pickle.load(open('{}.pickle.dat'.format(model_name), 'rb'))
-    loaded_scaler = pickle.load(open('{}_{}.pickle.dat'.format(learning_type, scaler_type), 'rb'))
-
-    with open('param_{}.json'.format(model_name)) as json_file:
-        params = json.load(json_file)
-        features = params['features']
-
-    # Prepare input variables
-    original_asset = asset
-    renditions_list = list(renditions)
-    metrics_list = ['temporal_gaussian']
-
-    # Process and compare original asset against the provided list of renditions
-    start = time.clock()
-    asset_processor = video_asset_processor(original_asset, renditions_list, metrics_list, 4, do_profiling)
-    initialize_time = time.clock() - start
-
-    start = time.clock()
-    metrics_df = asset_processor.process()
-    process_time = time.clock() - start
-
-    # Cleanup the resulting pandas dataframe and convert it to a numpy array
-    # to pass to the prediction model
-    for column in metrics_df.columns:
-        if 'series' in column:
-            metrics_df = metrics_df.drop([column], axis=1)
-
-    features.remove('attack_ID')
-
-    metrics_df = metrics_df[features]
-    metrics_df = metrics_df.drop('title', axis=1)
-    metrics_df = metrics_df.drop('attack', axis=1)
-    X = np.asarray(metrics_df)
-    # Scale data:
-    X = loaded_scaler.transform(X)
-
-    matrix = pickle.load(open('reduction_{}.pickle.dat'.format(model_name), 'rb'))
-    X = matrix.transform(X)
-
-    # Make predictions for given data
-    start = time.clock()
-    y_pred = loaded_model.predict(X)
-    prediction_time = time.clock() - start
-
-    # Display predictions
->>>>>>> Removes unneeded features from cli.py
     i = 0
     for rendition_uri in renditions:
         results.append({rendition_uri: predictions[i]})
