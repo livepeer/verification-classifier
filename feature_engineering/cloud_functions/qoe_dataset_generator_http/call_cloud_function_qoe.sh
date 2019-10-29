1b3056e0e27b7ada7fed992b7527918dc8967799
Rabindranath <rabindranath.andujar@gmail.com>
2019-10-29 09:35:45 +0100

    Adapts call script for qoe cloud function

---
+++ feature_engineering/cloud_functions/qoe_dataset_generator_http/call_cloud_function_qoe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for filename in $(gsutil ls gs://livepeer-qoe-sources/vimeo)
+do
+    # Define a timestamp function
+ 
+    filename="${filename##*/}"
+    gcloud functions call dataset_generator_qoe_http --data '{"name":"'"vimeo/$filename"'"}' &
+
+    echo "$(date)" "$filename";
+    sleep 7
+done;
