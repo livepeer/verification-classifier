a58723b0aaac52ebe9bf739b8edc716e9208736c
Rabindranath <rabindranath.andujar@gmail.com>
2020-01-30 13:36:47 +0100

    Adds test for timestamp seeking

--- testing/video_parser.py
+++ testing/video_parser.py
@@ -14,25 +14,38 @@ import streamlit as st
 from scipy.special import gamma as tgamma
 
 
-cap = cv2.VideoCapture('../stream/sources/vimeo/104681073-no-audio.mp4')
-cap2 = cv2.VideoCapture('../stream/sources/vimeo/104681073.mp4')
+cap = cv2.VideoCapture('../stream/sources/bbb_sunflower_1080p_30fps_normal_t02.mp4')
+cap2 = cv2.VideoCapture('../stream/sources/official_test_source_2s_keys_24pfs.mp4')
 
-start_time = time.time()
-i = 0
-while cap.isOpened():
-    ret1, frame1 = cap.read()
-    # ret2, frame2 = cap2.read()
-    if ret1:# and ret2:
-        i += 1
-        # res = cv2.quality.QualityMSE_compute(frame1, frame2)
-        if i == 5:
-            features = np.empty([36,])
-            # cv2.imshow('Frame', frame1)
-            # cv2.waitKey(0)
-            features = cv2.quality.QualityBRISQUE_computeFeatures(frame1, features) # specify model_path and range_path
-            i = 0        
-        # print(i, score)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+timestamps = [cap.get(cv2.CAP_PROP_POS_MSEC)]
+calc_timestamps = [0.0]
+time_stamp_counter = 0
+frame_counter = 0
+max_frames = int(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
+max_samples = int(max_frames * 0.01)
+frame_list = np.asarray(sorted(list(np.random.choice(max_frames,
+                                          max_samples,
+                                          replace=False)))) / fps
+start = time.time()
+print('Parsing {} out of {} frames'.format(max_samples, max_frames))
+while(cap.isOpened()):
+    frame_exists, curr_frame = cap.read()
+    if frame_exists:
+        frame_counter += 1     
+        if frame_counter / fps in frame_list:
+            print(frame_counter / fps)    
     else:
         break
-print(features)
-print('Elapsed time:', time.time() - start_time)
+print('Time to parse:', time.time() - start)
+
+start = time.time()
+for time_stamp in frame_list:
+    cap.set(cv2.CAP_PROP_POS_MSEC, time_stamp)
+    ret, frame = cap.read()
+    print(time_stamp)
+print('Time to parse:', time.time() -start)
+
+cap.release()
