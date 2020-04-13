ab998e695a27de613536a299978f70a0e367ccd1
Rabindranath <rabindranath.andujar@gmail.com>
2020-04-13 10:29:52 +0200

    Adds name to verifier-api docker image

--- launch_api.sh
+++ launch_api.sh
@@ -1,1 +1,2 @@
-docker build -f Dockerfile -t verifier:v1 . && docker build -f Dockerfile-api -t verifier-api:v1 . && docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs -p 5000:5000 verifier-api:v1
+docker rm verifier-api &&
+docker build -f Dockerfile -t verifier:v1 . && docker build -f Dockerfile-api -t verifier-api:v1 . && docker run --volume="$(pwd)/stream":/stream --volume="$(pwd)/logs":/logs -p 5000:5000 --name verifier-api verifier-api:v1
