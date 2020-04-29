3cf436d12718c00438de4f67bdee6a97063ba879
Rabindranath <rabindranath.andujar@gmail.com>
2020-04-29 13:29:49 +0200

    Updates model file for test_api script

---
+++ testing/test_api.sh
@@ -0,0 +1,8 @@
+for filename in $(ls ../stream/sources)
+do
+    # Define a timestamp function
+    filename="${filename##*/}"
+    curl localhost:5000/verify -d '{"source":"stream/sources/'$filename'","renditions":[{"uri":"stream/720p_black_and_white/_AN9U4D1Cww.mp4"},{"uri":"stream/240p_flip_horizontal/'$filename'"}],"orchestratorID": "foo","model": "https://storage.googleapis.com/verification-models/verification-metamodel.tar.xz"}' -H 'Content-Type: application/json' &
+    echo "$(date)" "$filename";
+    sleep 1
+done;
