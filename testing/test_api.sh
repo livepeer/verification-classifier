4c27bc718c8d650424ed2f3a5f561d401e4de4c9
Rabindranath <rabindranath.andujar@gmail.com>
2020-04-30 10:47:52 +0200

    Changes test_api to get data from bucket

--- testing/test_api.sh
+++ testing/test_api.sh
@@ -1,8 +1,8 @@
-for filename in $(ls ../stream/sources)
+for filename in $(gsutil ls gs://livepeer-qoe-sources/vimeo)
 do
     # Define a timestamp function
     filename="${filename##*/}"
-    curl localhost:5000/verify -d '{"source":"stream/sources/'$filename'","renditions":[{"uri":"stream/720p_black_and_white/_AN9U4D1Cww.mp4"},{"uri":"stream/240p_flip_horizontal/'$filename'"}],"orchestratorID": "foo","model": "https://storage.googleapis.com/verification-models/verification-metamodel.tar.xz"}' -H 'Content-Type: application/json' &
     echo "$(date)" "$filename";
+    curl localhost:5000/verify -d '{"source":"https://storage.googleapis.com/livepeer-qoe-sources/vimeo/'$filename'","renditions":[{"uri":"https://storage.googleapis.com/livepeer-qoe-renditions/720_14/vimeo/'$filename'"}],"orchestratorID": "foo","model": "https://storage.googleapis.com/verification-models/verification-metamodel.tar.xz"}' -H 'Content-Type: application/json' &
     sleep 1
 done;
