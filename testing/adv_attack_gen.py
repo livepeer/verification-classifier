43a86ea4ef0a74956833ae7b124e925523543643
Ivan Poleshchuk <ivan.poleshchuk@outlook.com>
2020-06-26 19:19:30 +0500

    Channel-based attack example

---
+++ testing/adv_attack_gen.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    src = 'tests/data/master_4s_1080.mp4'
+    cap = cv2.VideoCapture(src)
+    writer = cv2.VideoWriter('rend2_4s_1080_adv_attack.mp4', cv2.VideoWriter_fourcc(*'H264'), 60, (1920, 1080))
+    while True:
+        _, frame = cap.read()
+        if frame is None:
+            break
+        frame = frame[..., ::-1]
+        frame[..., 0] = np.max(frame, axis=2)
+        frame[..., 1:] = 0
+        writer.write(frame)
+    cap.release()
+    writer.release()
