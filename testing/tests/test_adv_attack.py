e56cb3ab6e9ebc4d3d7f5ac1ebc41ff66b460cac
Ivan Poleshchuk <ivan.poleshchuk@outlook.com>
2020-07-06 19:41:32 +0500

    Meta-model logic update, Verifier model download logic fix, new model archive URI

--- testing/tests/test_adv_attack.py
+++ testing/tests/test_adv_attack.py
@@ -23,7 +23,7 @@ class TestAdvAttack:
         debug = True
         src = 'testing/tests/data/master2_4s_1080.mp4'
         rend_path = 'testing/tests/data/rend2_4s_1080_adv_attack.mp4'
-        verifier = Verifier(10, 'http://storage.googleapis.com/verification-models/verification-metamodel-fps2.tar.xz', False, False, debug)
+        verifier = Verifier(10, 'http://storage.googleapis.com/verification-models/verification-metamodel-2020-07-06.tar.xz', False, False, debug)
         verification_result = verifier.verify(src, [{'uri': rend_path}])
         print(verification_result)
         assert verification_result[0]['tamper'] == 1
