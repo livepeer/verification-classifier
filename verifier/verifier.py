641bf98975219987db44939c595f248033b13bb2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-27 15:19:25 +0000

    Adding tarfile member sanitization to extractall()

--- verifier/verifier.py
+++ verifier/verifier.py
@@ -250,7 +250,26 @@ class Verifier:
                                                              filename='{}/{}'.format(model_dir, model_file))
                     logger.info(f'Model {filename} downloaded')
                     with tarfile.open(filename) as tar_f:
-                        tar_f.extractall(model_dir)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tar_f, model_dir)
 
                     return model_dir, model_file, model_file_sl
                 except Exception as exc:
